8ad639c1907af5c67b9a75871a10ca76fc8e2697
Mal <malrsteffes@gmail.com>
2019-10-08 17:19:39 -0400

    html Route page code started

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,22 @@
+// DEPENDENCIES =====================================================
+// We need the path package to get the correct filepath for our html page
+
+var path = require("path");
+
+
+// ROUTING =====================================================
+module.exports = function(app){
+    // These are our HTML get requests
+
+    //Home
+    app.get("/home", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    })
+
+    //The survey
+    app.get("/survey", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    })
+
+
+}
