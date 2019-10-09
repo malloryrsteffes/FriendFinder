7be5ed6bed42fc55a2b4967e6fe72dce61d1dedf
Mal <malrsteffes@gmail.com>
2019-10-09 17:19:59 -0400

    added a few hard-coded friends for comparison purposes. added js to the apiRoute that currently finds the difference score between the user input and each friend's scores in the friendArray

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,38 @@
 // We start with an empty array of friends
-var friendArray = [];
+var friendArray = [
+    {
+        "name": "Mal",
+        "photo": "google.com",
+        "scores": [
+        1,
+        5,
+        5,
+        1,
+        3,
+        5,
+        4,
+        5,
+        5,
+        5
+        ]
+        },
+    {
+        "name": "Bob",
+        "photo": "bobross.com",
+        "scores": [
+        3,
+        4,
+        2,
+        1,
+        1,
+        5,
+        5,
+        2,
+        2,
+        3
+        ]
+        }
+];
 
 // Export the array. This makes it accessible to other files using require.
 module.exports = friendArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,7 +13,37 @@ module.exports = function(app){
 
     // POST
     app.post("/api/friends", function(req, res){
-        
-        friendArray.push(req.body);
+
+        // grab our newFriend data
+        var newFriend = req.body;
+        // turning the string of scores into numbers
+        for(var i = 0; i < newFriend.scores.length; i++) {
+            newFriend.scores[i] = parseInt(newFriend.scores[i]);
+          }
+
+        console.log(newFriend.scores);
+
+        // We need to find a way to find the absolute value between two numbers. 
+        // Sample code pulled from Mozilla:
+        // function difference(a, b) {
+        //     return Math.abs(a - b);
+        //   }
+
+        // Loop through the friends array
+        for (i = 0; i < friendArray.length; i++){
+            // Set a starting difference of zero
+            var difference = 0;
+            // Loop through the scores from the user
+            for (j = 0; j < newFriend.scores.length; j++){
+                // use the math.abs function to find the difference between the user scores 
+                //and the scores from each friend the friendsArray
+                difference += Math.abs(friendArray[i].scores[j] - newFriend.scores[j]);
+
+            }
+            // IT WORKS KINDA
+            console.log(friendArray[i].name + ' difference = ' + difference);
+
+        }
+        friendArray.push(newFriend);
     })
 }
