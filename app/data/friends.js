3dce5ffbe4232b712ef8caea175f0e108f8e8e4f
Mal <malrsteffes@gmail.com>
2019-10-09 20:49:11 -0400

    fleshed out available friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,50 +4,114 @@ var friendArray = [
         "name": "Mal S",
         "photo": "https://user-images.githubusercontent.com/52713263/66530107-5d40b980-ead4-11e9-80a1-2f3a91ed02a8.png",
         "scores": [
-        1,
-        5,
-        5,
-        1,
-        3,
-        5,
-        4,
-        5,
-        5,
-        5
+            1,
+            3,
+            5,
+            1,
+            3,
+            5,
+            4,
+            5,
+            5,
+            5
         ]
-        },
+    },
     {
         "name": "Bob Ross",
         "photo": "https://dynaimage.cdn.cnn.com/cnn/q_auto,w_412,c_fill,g_auto,h_412,ar_1:1/http%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F160523192458-10-netflix-amazon-hulu-itunes-june.jpg",
         "scores": [
-        3,
-        4,
-        2,
-        1,
-        1,
-        5,
-        5,
-        2,
-        2,
-        3
+            3,
+            4,
+            2,
+            1,
+            1,
+            5,
+            5,
+            2,
+            2,
+            3
         ]
-        },
+    },
     {
         "name": "Zeke Elliott",
         "photo": "https://res-2.cloudinary.com/ybmedia/image/upload/c_scale,f_auto,q_auto,w_0.55/v1/m/6/b/6bdc1d17079d7e53129c5bd380b0a3ecb3f2bc1b/thumb_16x9/10227552.jpg",
         "scores": [
-        5,
-        1,
-        5,
-        2,
-        5,
-        2,
-        1,
-        5,
-        3,
-        4
+            5,
+            2,
+            5,
+            2,
+            5,
+            2,
+            1,
+            5,
+            3,
+            4
         ]
-        }
+    },
+    {
+        "name": "Optimus Prime",
+        "photo": "https://d13ezvd6yrslxm.cloudfront.net/wp/wp-content/images/transformers-optimus-darkmoon-city-700x334.jpg",
+        "scores": [
+            5,
+            5,
+            5,
+            2,
+            5,
+            2,
+            1,
+            5,
+            3,
+            4
+        ]
+    },
+    {
+        "name": "Dana Scully",
+        "photo": "https://www.flare.com/wp-content/uploads/2016/01/dana-scully.jpg",
+        "scores": [
+            2,
+            5,
+            4,
+            4,
+            3,
+            5,
+            3,
+            1,
+            5,
+            1
+        ]
+    },
+    {
+        "name": "Obi-Wan Kenobi",
+        "photo": "http://3.bp.blogspot.com/-Fdf6ws_zuIo/TtkMRMx7MFI/AAAAAAAANbU/7LK-glgoz04/s1600/obi.jpg",
+        "scores": [
+            3,
+            3,
+            4,
+            4,
+            5,
+            3,
+            5,
+            3,
+            4,
+            4
+        ]
+    },
+    {
+        "name": "Baker Mayfield",
+        "photo": "https://i.kinja-img.com/gawker-media/image/upload/s--A33wZptn--/c_scale,f_auto,fl_progressive,q_80,w_800/ul0lyenprhujmka3vpe5.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            1,
+            5,
+            5,
+            1,
+            1,
+            1
+        ]
+    },
 ];
 
 // Export the array. This makes it accessible to other files using require.
